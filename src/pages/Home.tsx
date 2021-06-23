a72aa0d11f8f3441dadf63322c9d64307ecfeeb2
Gybson Klarck <gybsonklarck@gmail.com>
2021-06-22 21:07:30 -0300

    Format document with prettier

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,40 @@
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
 
-import '../styles/auth.scss';
-import { Button } from '../components/Button';
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
 
 export function Home() {
-    const history = useHistory();
+  const history = useHistory();
 
-    function navigateToNewRoom() {
-        history.push('/rooms/new');
-    }
+  function navigateToNewRoom() {
+    history.push("/rooms/new");
+  }
 
-    return (
-        <div id="page-auth">
-            <aside>
-                <img src={illustrationImg} alt="Ilustração" />
-                <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as dúvidas da sua audiência em tempo real</p>
-            </aside>
-            <main>
-                <div className="main-content">
-                    <img src={logoImg} alt="Letmeask" />
-                    <button onClick={navigateToNewRoom} className="create-room">
-                        <img src={googleIconImg} alt="Google" />
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">ou entre em uma sala</div>
-                    <form>
-                        <input 
-                            type="text" 
-                            placeholder="Digite o código da sala"
-                        />
-                        <Button type="submit">
-                            Entrar na sala
-                        </Button>
-                    </form>
-                </div>
-            </main>
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button onClick={navigateToNewRoom} className="create-room">
+            <img src={googleIconImg} alt="Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
