04a14d54d2d396bc1f39aad1ca26af92b5a45354
Gybson Klarck <gybsonklarck@gmail.com>
2021-06-22 23:51:37 -0300

    Authentication with firebase and google

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home}/>
-      <Route path="/rooms/new" component={NewRoom}/>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home}/>
+        <Route path="/rooms/new" component={NewRoom}/>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
+
 
 export default App;
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
 
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
+// import { useAuth } from "../hooks/useAuth";
 
 export function NewRoom() {
+  // const { user } = useAuth();
+
   return (
     <div id="page-auth">
       <aside>
